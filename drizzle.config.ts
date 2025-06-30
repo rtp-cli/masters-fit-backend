2f946d52f2c1d2dd80744e7392129ee0be593de0
Areej Naqvi <areej.faisal@conradlabs.com>
2025-06-30 13:31:45 +0500

    config: fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default defineConfig({
-  schema: "./src/models/index.ts",
+  schema: "./api/models/index.ts",
   out: "./migrations",
   driver: "pg",
   dbCredentials: {
