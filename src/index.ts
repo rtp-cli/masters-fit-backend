99ab613aa77a47e16c5f524f49bbf3c1b2089e34
Areej Naqvi <areej.faisal@conradlabs.com>
2025-06-30 12:02:26 +0500

    feat: update index file for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import app from "./app";
 
-const port = parseInt(process.env.PORT || "5000", 10);
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  console.log(`Swagger docs: http://localhost:${port}/api/docs`);
-});
+export default app;
